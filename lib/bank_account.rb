07b3f05b61f482584982592bd83461935368ce75
Huseyin Cicek <htcicek@users.noreply.github.com>
2019-09-19 14:55:27 -0400

    transfer execute compelted

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,32 +1,27 @@
 class BankAccount
 
-attr_accessor :balance, :status
-attr_reader :name
+  attr_accessor :balance, :status
+  attr_reader :name
 
-def initialize(name)
-  @name = name
-  @balance = 1000
-  @status = "open"
-end
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
 
-def deposit(num)
-  @balance += num
-end
+  def deposit(num)
+    @balance += num
+  end
 
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
 
-def display_balance
-  "Your balance is $#{@balance}."
-end
+  def valid?
+    @balance > 0 && @status == "open"
+  end
 
-
-def valid?
-  @balance > 0 && @status == "open"
-end
-
-
-def close_account
-  @status = "closed"
-end
-
-
-end
+  def close_account
+    @status = "closed"
+  end
+end # End bank account class
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Transfer
-  # your code here
-end
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    # binding.pry
+    (self.valid? && @status != 'complete' && @sender.balance >= @amount) ? (@sender.deposit(-@amount); @receiver.deposit(@amount); @status = 'complete') : (@status = 'rejected';'Transaction rejected. Please check your account balance.')
+  end
+end # End transfer class
